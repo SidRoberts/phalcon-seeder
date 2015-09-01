6065869ddcf091440f2d6864622435407fcf9852
Sid Roberts <sid@sidroberts.co.uk>
2015-09-01 15:48:37 +0100

    Moved annotations parser to its own class.

--- src/Seeder.php
+++ src/Seeder.php
@@ -45,11 +45,13 @@ class Seeder extends \Phalcon\Di\Injectable implements \Phalcon\Events\EventsAwa
                     $eventsManager->fire("seeder:beforeCreateTable", $model);
                 }
 
+                $modelAnnotations = new \Sid\Phalcon\Seeder\Annotations($model);
+
                 $success = $this->db->createTable(
                     $model->getSource(),
                     null,
                     [
-                        "columns" => $this->getColumns($model)
+                        "columns" => $modelAnnotations->getColumns()
                     ]
                 );
 
@@ -63,7 +65,9 @@ class Seeder extends \Phalcon\Di\Injectable implements \Phalcon\Events\EventsAwa
             }
 
             foreach ($models as $model) {
-                $indexes = $this->getIndexes($model);
+                $modelAnnotations = new \Sid\Phalcon\Seeder\Annotations($model);
+
+                $indexes = $modelAnnotations->getIndexes();
 
                 if (!$indexes) {
                     continue;
@@ -89,7 +93,9 @@ class Seeder extends \Phalcon\Di\Injectable implements \Phalcon\Events\EventsAwa
             }
 
             foreach ($models as $model) {
-                $references = $this->getReferences($model);
+                $modelAnnotations = new \Sid\Phalcon\Seeder\Annotations($model);
+
+                $references = $modelAnnotations->getReferences();
 
                 if (!$references) {
                     continue;
@@ -115,7 +121,9 @@ class Seeder extends \Phalcon\Di\Injectable implements \Phalcon\Events\EventsAwa
             }
 
             foreach ($models as $model) {
-                $data = $this->getInitialData($model);
+                $modelAnnotations = new \Sid\Phalcon\Seeder\Annotations($model);
+
+                $data = $modelAnnotations->getInitialData();
 
                 if (!$data) {
                     continue;
@@ -163,7 +171,9 @@ class Seeder extends \Phalcon\Di\Injectable implements \Phalcon\Events\EventsAwa
             $this->db->begin();
 
             foreach ($models as $model) {
-                $references = $this->getReferences($model);
+                $modelAnnotations = new \Sid\Phalcon\Seeder\Annotations($model);
+
+                $references = $modelAnnotations->getReferences();
 
                 if (!$references) {
                     continue;
@@ -216,182 +226,5 @@ class Seeder extends \Phalcon\Di\Injectable implements \Phalcon\Events\EventsAwa
 
             throw $e;
         }
-    }
-
-
-
-    /**
-     * @param  \Phalcon\Mvc\ModelInterface $model
-     *
-     * @return array
-     */
-    public function getColumns(\Phalcon\Mvc\ModelInterface $model)
-    {
-        $columns = [];
-
-        $propertiesAnnotations = $this->annotations->getProperties(get_class($model));
-
-        foreach ($propertiesAnnotations as $property => $propertyAnnotations) {
-            if (!$propertyAnnotations->has('Column')) {
-                continue;
-            }
-
-            $definition = [];
-
-            $definition['primary']       = $propertyAnnotations->has("Primary");
-            $definition['autoIncrement'] = $propertyAnnotations->has("Identity");
-
-            $columnAnnotation = $propertyAnnotations->get("Column");
-
-            $columnName = $columnAnnotation->getNamedParameter("column");
-            if ($columnName === null) {
-                $columnName = $property;
-            }
-
-            $type = $columnAnnotation->getNamedParameter("type");
-            $definition['type'] = $this->getColumnTypeConstant($type);
-
-            $nullable = $columnAnnotation->getNamedParameter("nullable");
-            if ($nullable !== null) {
-                $definition['notNull'] = !$nullable;
-            }
-
-            $default = $columnAnnotation->getNamedParameter("default");
-            if ($default !== null) {
-                $definition["default"] = $default;
-            }
-
-            $size = $columnAnnotation->getNamedParameter("size");
-            if ($size !== null) {
-                $definition['size'] = $size;
-            }
-
-            $columns[] = new \Phalcon\Db\Column($columnName, $definition);
-        }
-
-        return $columns;
-    }
-
-    /**
-     * @param  \Phalcon\Mvc\ModelInterface $model
-     *
-     * @return array
-     */
-    public function getIndexes(\Phalcon\Mvc\ModelInterface $model)
-    {
-        $indexes = [];
-
-        $classAnnotations = $this->annotations->get(get_class($model))->getClassAnnotations();
-
-        if ($classAnnotations) {
-            if ($classAnnotations->has("Index")) {
-                $indexAnnotations = $classAnnotations->getAll("Index");
-
-                foreach ($indexAnnotations as $indexAnnotation) {
-                    $arguments = $indexAnnotation->getArguments();
-
-                    $name    = $arguments[0];
-                    $columns = $arguments[1];
-                    $type    = isset($arguments[2]) ? $arguments[2] : null;
-
-                    $indexes[] = new \Phalcon\Db\Index($name, $columns, $type);
-                }
-            }
-        }
-
-        return $indexes;
-    }
-
-    /**
-     * @param  \Phalcon\Mvc\ModelInterface $model
-     *
-     * @return array
-     */
-    public function getReferences(\Phalcon\Mvc\ModelInterface $model)
-    {
-        $references = [];
-
-        $classAnnotations = $this->annotations->get(get_class($model))->getClassAnnotations();
-
-        if ($classAnnotations) {
-            if ($classAnnotations->has("Reference")) {
-                $referenceAnnotations = $classAnnotations->getAll("Reference");
-
-                foreach ($referenceAnnotations as $referenceAnnotation) {
-                    $arguments = $referenceAnnotation->getArguments();
-
-                    $references[] = new \Phalcon\Db\Reference($arguments[0], $arguments[1]);
-                }
-            }
-        }
-
-        return $references;
-    }
-
-    /**
-     * @param  \Phalcon\Mvc\ModelInterface $model
-     *
-     * @return array
-     */
-    public function getInitialData(\Phalcon\Mvc\ModelInterface $model)
-    {
-        $data = [];
-
-        $classAnnotations = $this->annotations->get(get_class($model))->getClassAnnotations();
-
-        if ($classAnnotations) {
-            if ($classAnnotations->has("Data")) {
-                $dataAnnotations = $classAnnotations->getAll("Data");
-
-                foreach ($dataAnnotations as $dataAnnotation) {
-                    $data[] = $dataAnnotation->getArgument(0);
-                }
-            }
-
-            if ($classAnnotations->has("DataJson")) {
-                $guzzle = new \GuzzleHttp\Client();
-
-                $dataJsonAnnotations = $classAnnotations->getAll("DataJson");
-
-                foreach ($dataJsonAnnotations as $dataJsonAnnotation) {
-                    $url = $dataJsonAnnotation->getArgument(0);
-
-                    $response = $guzzle->get($url);
-
-                    $json = json_decode($response->getBody(), true);
-
-                    foreach ($json as $datum) {
-                        $data[] = $datum;
-                    }
-                }
-            }
-        }
-
-        return $data;
-    }
-
-    protected function getColumnTypeConstant($type)
-    {
-        $columnTypes = [
-            "integer"    => \Phalcon\Db\Column::TYPE_INTEGER,
-            "date"       => \Phalcon\Db\Column::TYPE_DATE,
-            "varchar"    => \Phalcon\Db\Column::TYPE_VARCHAR,
-            "decimal"    => \Phalcon\Db\Column::TYPE_DECIMAL,
-            "datetime"   => \Phalcon\Db\Column::TYPE_DATETIME,
-            "char"       => \Phalcon\Db\Column::TYPE_CHAR,
-            "text"       => \Phalcon\Db\Column::TYPE_TEXT,
-            "float"      => \Phalcon\Db\Column::TYPE_FLOAT,
-            "boolean"    => \Phalcon\Db\Column::TYPE_BOOLEAN,
-            "double"     => \Phalcon\Db\Column::TYPE_DOUBLE,
-            "tinyblob"   => \Phalcon\Db\Column::TYPE_TINYBLOB,
-            "blob"       => \Phalcon\Db\Column::TYPE_BLOB,
-            "mediumblob" => \Phalcon\Db\Column::TYPE_MEDIUMBLOB,
-            "longblob"   => \Phalcon\Db\Column::TYPE_LONGBLOB,
-            "biginteger" => \Phalcon\Db\Column::TYPE_BIGINTEGER,
-            "json"       => \Phalcon\Db\Column::TYPE_JSON,
-            "jsonb"      => \Phalcon\Db\Column::TYPE_JSONB
-        ];
-
-        return $columnTypes[$type];
     }
 }
